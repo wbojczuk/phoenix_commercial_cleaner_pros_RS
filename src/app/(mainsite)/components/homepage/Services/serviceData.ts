42576c948ef2a169950948086fdba5d3cc003e85
William Bojczuk <williambojczuk@gmail.com>
2024-11-14 23:45:57 -0500

    updated to webp

--- src/app/(mainsite)/components/homepage/Services/serviceData.ts
+++ src/app/(mainsite)/components/homepage/Services/serviceData.ts
@@ -8,7 +8,7 @@ interface Service{
 const serviceData: Service[] = [
     
     {
-        img: "./img/services/janitor.png",
+        img: "./img/services/janitor.webp",
         name: "Janitorial Services",
         shortDesc: " Keep your business clean and welcoming with our reliable janitorial services, designed to handle everyday cleaning needs and create a comfortable work environment for employees and visitors alike.",
         desc: ` Keep your business clean and welcoming with our reliable janitorial services, designed to handle everyday cleaning needs and create a comfortable work environment for employees and visitors alike.`
@@ -20,13 +20,13 @@ const serviceData: Service[] = [
         desc: `Our residential cleaning services ensure every room in your home is thoroughly cleaned, leaving you with a fresh and spotless living space you can enjoy.`
     },
     {
-        img: "./img/services/moving.png",
+        img: "./img/services/moving.webp",
         name: "Move-in & Move-out Cleaning",
         shortDesc: "Make moving a breeze with our detailed move-in and move-out cleaning services, which ensure each space is clean, sanitized, and ready for its next occupant.",
         desc: `Make moving a breeze with our detailed move-in and move-out cleaning services, which ensure each space is clean, sanitized, and ready for its next occupant.`
     },
     {
-        img: "./img/services/window.png",
+        img: "./img/services/window.webp",
         name: "Window Cleaning",
         shortDesc: "Window cleaning is a professional service that ensures your windows are spotless and streak-free, enhancing the appearance of your home or business.",
         desc: `Window cleaning is a professional service that ensures your windows are spotless and streak-free, enhancing the appearance of your home or business.
@@ -34,19 +34,19 @@ const serviceData: Service[] = [
 Our team uses specialized tools and techniques to remove dirt, grime, and smudges, allowing more natural light to brighten your space. Trust us to keep your windows looking their best, providing a clear and beautiful view.`
     },
     {
-        img: "./img/services/upholstery.png",
+        img: "./img/services/upholstery.webp",
         name: "Upholstery Cleaning",
         shortDesc: "Refresh your furniture with our upholstery cleaning service, effectively removing dirt, stains, and allergens to make your couches, chairs, and more look like new.",
         desc: `Refresh your furniture with our upholstery cleaning service, effectively removing dirt, stains, and allergens to make your couches, chairs, and more look like new.`
     },
     {
-        img: "./img/services/carpet.png",
+        img: "./img/services/carpet.webp",
         name: "Carpet Cleaning",
         shortDesc: "Our carpet cleaning service deep-cleans carpets to remove embedded dirt, stains, and odors, restoring a fresh and inviting look to any room.",
         desc: `Our carpet cleaning service deep-cleans carpets to remove embedded dirt, stains, and odors, restoring a fresh and inviting look to any room.`
     },
     {
-        img: "./img/services/floor.png",
+        img: "./img/services/floor.webp",
         name: "Floor Care Maintenance",
         shortDesc: "Extend the life and beauty of your floors with our floor care maintenance services, including cleaning, polishing, and specialized treatments for different floor types.",
         desc: `Extend the life and beauty of your floors with our floor care maintenance services, including cleaning, polishing, and specialized treatments for different floor types.`
